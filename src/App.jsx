1a889cd2807cb47f5b375bb4bf2c23d545bf2eab
vedkulk <kulkarnived3@gmail.com>
2025-08-05 22:51:10 +0530

    routes, db and component setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AppLayout from './layouts/AppLayout'
+import LandingPage from './pages/LandingPage'
+import Dashboard from './pages/Dashboard'
+import Auth from './pages/Auth'
+import Link from './pages/Link'
+import RedirectLink from './pages/RedirectLink'
+
+const router = createBrowserRouter([
+  {
+    element:<AppLayout/>,
+    children:[
+      {
+        path:'/',
+        element:<LandingPage/>
+      },
+      {
+        path:'/dashboard',
+        element:<Dashboard/>
+      },
+      {
+        path:'/auth',
+        element:<Auth/>
+      },
+      {
+        path:'/link/:id',
+        element:<Link/>
+      },
+      {
+        path:'/:id',
+        element:<RedirectLink/>
+      }
+    ]
+  }
+])
 
 const App = () => {
-  return (
-    <div className='test-2xl '>App</div>
-  )
+  return <RouterProvider router={router}/>
 }
 
 export default App
